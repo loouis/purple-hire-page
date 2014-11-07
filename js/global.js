990e2d98154263badd5b7dd53b5ea6f6f5b767fe
Louis <louis@ahoia.com>
2014-11-07 16:20:42 +0000

    menu fix, change people text

--- js/global.js
+++ js/global.js
@@ -6,19 +6,19 @@
 	});
 
 
-  $('a[href*=#]:not([href=#])').click(function(e) {
-  	e.preventDefault();
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top -135
-        }, 1000);
-        return false;
-      }
-    }
-  });
+  // $('a[href*=#]:not([href=#])').click(function(e) {
+  // 	e.preventDefault();
+  //   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+  //     var target = $(this.hash);
+  //     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+  //     if (target.length) {
+  //       $('html,body').animate({
+  //         scrollTop: target.offset().top -135
+  //       }, 1000);
+  //       return false;
+  //     }
+  //   }
+  // });
 
 
 
